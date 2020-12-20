452b6334bd4eaa371753ae949edb62239ddc1d06
echenim <myron.echenim@gmail.com>
2020-12-20 01:24:19 -0800

    IRoute contract defination

--- handlers/http_route_interface.go
+++ handlers/http_route_interface.go
@@ -1,5 +1,11 @@
 package handlers
 
+import (
+	"net/http"
+)
+
 //IRoute interface defination
 type IRoute interface {
+	GET(uri string, f func(resp http.ResponseWriter, req *http.Request))
+	SERVE()
 }
